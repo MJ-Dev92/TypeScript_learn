9bdef9484010dfba528249adaa2885c7a2eb0e1e
MJ-Dev92 <minjae920802@naver.com>
2024-05-01 23:20:30 +0900

    05 union

---
+++ Type-01/05-union.ts
@@ -0,0 +1,42 @@
+{
+  /* Union Types: OR */
+
+  type Direction = "left" | "right" | "up" | "down";
+  function move(direction: Direction) {
+    console.log(direction);
+  }
+  move("down"); // 'left' 'right' 'up' 'down' 중 하나만 사용할 수 있다.
+
+  type TileSize = 8 | 16 | 32;
+  const tile: TileSize = 16;
+
+  // function: login -> success, fail
+  type SuccessState = {
+    response: {
+      body: string;
+    };
+  };
+  type FailState = {
+    reason: string;
+  };
+  type LoginState = SuccessState | FailState;
+
+  function login(): LoginState {
+    return {
+      response: {
+        body: "logge in!",
+      },
+    };
+  }
+
+  // printLoginState(state: LoginState)
+  // success -> body
+  //fail -> reason
+  function printLoginState(state: LoginState) {
+    if ("response" in state) {
+      console.log(state.response.body);
+    } else {
+      console.log(state.reason);
+    }
+  }
+}
