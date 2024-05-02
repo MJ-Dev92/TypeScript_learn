1ad8a2ae943a900ea68031a89897963ca7a9eb88
MJ-Dev92 <minjae920802@naver.com>
2024-05-02 16:53:17 +0900

    direction quiz

--- Type-Projects-Quiz/game.ts
+++ Type-Projects-Quiz/game.ts
@@ -1,6 +1,26 @@
 /**
  * Let's make a game s
  */
+
+const position = {
+  x: 0,
+  y: 0,
+};
+
+type Direction = "up" | "down" | "left" | "right";
+function move(direction: Direction) {
+  if (direction === "up") {
+    position.y += 1;
+  } else if (direction === "down") {
+    position.y -= 1;
+  } else if (direction === "left") {
+    position.x -= 1;
+  } else if (direction === "right") {
+    position.x += 1;
+  } else {
+    throw Error(`wrong position ${direction}`);
+  }
+}
 console.log(position); // { x: 0, y: 0}
 move("up");
 console.log(position); // { x: 0, y: 1}
