dd031836f65e56bcb02bcbb0dd7b437be5a20baa
MJ-Dev92 <minjae920802@naver.com>
2024-05-03 15:33:29 +0900

    abstraction

---
+++ Type-02-OOP/04-abstraction.ts
@@ -0,0 +1,62 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans must be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    private grindBeans(shots: number) {
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up...");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pullin ${shots} shots... `);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  maker.fillCoffeeBeans(32);
+  maker.makeCoffee(2);
+  2;
+  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
+  // maker2.fillCoffeeBeans(32);  <- 인터페이스 안에 관련 함수가 없기 때문에 사용할 수없다
+  maker2.makeCoffee(2);
+}
