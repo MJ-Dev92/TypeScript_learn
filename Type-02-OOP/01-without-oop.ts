5ac64378181b87109d9234311c5b98af30644d11
MJ-Dev92 <minjae920802@naver.com>
2024-05-30 22:39:33 +0900

    composition

--- Type-02-OOP/01-without-oop.ts
+++ Type-02-OOP/01-without-oop.ts
@@ -2,28 +2,24 @@
   // 전역적으로 커피콩을 가지고 있는 변수를 만들고
   // 그거를 가지고 커피를 만드는 함수를 만들자.
   // 커피를 만드는 makecoffee함수에는 인자 shot을 가지고 있다.
+
   type CoffeeCup = {
     shots: number;
     hasMilk: boolean;
   };
-
   const BEANS_GRAMM_PER_SHOT: number = 7;
   let coffeeBeans: number = 0;
-
   function makeCoffee(shots: number): CoffeeCup {
     if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
       throw new Error("Not enough coffee beans!");
     }
-
     coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
     return {
       shots,
       hasMilk: false,
     };
   }
-
   coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
-
   const coffee = makeCoffee(2);
   console.log(coffee);
 }
