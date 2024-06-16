d6d8b753ca4421172cc5685cb6af606c4606b628
MJ-Dev92 <minjae920802@naver.com>
2024-06-16 21:34:18 +0900

    TS-generic

--- Type-04-Generic/03-constrains.ts
+++ Type-04-Generic/03-constrains.ts
@@ -28,3 +28,20 @@ bob.workPartTime();
 
 const mjAfterPay = pay(mj);
 const bobAfterPay = pay(bob);
+
+const obj = {
+  name: "MJ",
+  age: 20,
+};
+
+const obj2 = {
+  animal: "Dog",
+};
+
+console.log(getValue(obj, "name")); // MJ
+console.log(getValue(obj, "age")); // 20
+console.log(getValue(obj2, "animal")); // Dog
+
+function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
