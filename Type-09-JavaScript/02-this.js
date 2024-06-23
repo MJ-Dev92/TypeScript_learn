e71fa1cb3bba8e58b18746d4f8d023a51b0f0dda
MJ-Dev92 <minjae920802@naver.com>
2024-06-23 22:39:40 +0900

    module

--- Type-09-JavaScript/02-this.js
+++ Type-09-JavaScript/02-this.js
@@ -8,13 +8,13 @@ console.clear();
 class Counter {
   count = 0;
   increase = () => {
-    // console.log(this);
+    console.log(this);
   };
 }
 const counter = new Counter();
 counter.increase();
 const caller = counter.increase;
-//const caller = counter.increase.bind(counter);
+// const caller = counter.increase.bind(counter);
 caller();
 
 class Bob {}
